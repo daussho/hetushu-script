739ded9550be80f030f96764b682f4f3e2394f57
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2024-09-23 11:01:14 +0700

    Update hetushu.user.js

--- hetushu.user.js
+++ hetushu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name hetushu font
 // @namespace http://daussho.com/
-// @version 0.5
+// @version 0.6
 // @description  Hetushu font
 // @match https://m.hetushu.com/book/*
 // @copyright 2024+, daussho.com
@@ -13,7 +13,9 @@
   await new Promise((r) => setTimeout(r, 1000));
 
   document.querySelector("#content").style =
-    "font-size: 22px; line-height: 1.5;";
+    "font-size: 24px; line-height: 1.5;";
   document.querySelector(".cmask").remove();
   document.querySelector("#message").remove();
+  document.querySelector("body").className = "day";
+  document.querySelector("body").setAttribute("data-type", 1);
 })();
